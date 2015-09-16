aa377974e8378468217cb142aae8cfb9bd6d1881
BleuAzur <seiwert@live.fr>
2015-09-16 16:35:12 +0200

    Passage en HTTPS - Partie 2
    Génération de clés et certificats dans le dossier private. -> HTTPS
    fonctionnel en navigateur

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+var https = require('https'),      // module for https
+fs =    require('fs');         // required to read certs and keys
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index.html');
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+var options = {
+    key:    fs.readFileSync('private/server.key'),
+    cert:   fs.readFileSync('private/server.crt'),
+    requestCert:        true,
+    rejectUnauthorized: false
+};
+
+https.createServer(options, function (req, res) {
+	console.log("Server running")
+    if (req.client.authorized) {
+        res.writeHead(200, '{&quot;Content-Type&quot;: &quot;application/json&quot;}');
+        res.end('{&quot;status&quot;:&quot;approved&quot;}');
+    } else {
+        res.writeHead(401, '{&quot;Content-Type&quot;: &quot;application/json&quot;}');
+        res.end('{&quot;status&quot;:&quot;denied&quot;}');
+    }
+}).listen(3000);
