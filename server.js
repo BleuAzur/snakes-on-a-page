f5af8ed72524bc3d6328a7b9cd66c2e2a3270b5e
BleuAzur <seiwert@live.fr>
2015-09-16 14:13:10 +0200

    Ajustements à server.js
    Server.js fonctionnel - Port 3000

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
-var oneDay = 86400000;
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-app.use(express.compress());
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
-
-app.listen(process.env.PORT || 3000);
+  console.log('Example app listening at http://%s:%s', host, port);
+});
