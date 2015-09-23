26c3fd7b10831226d4a36adf14aa61635ef12171
BleuAzur <seiwert@live.fr>
2015-09-23 12:09:11 +0200

    Installation de paper.js - Modif. code serveur
    Changements du code serveur, paper.js mis en place, intégration de
    quelques fonctions dans index.html

--- server.js
+++ server.js
@@ -3,25 +3,22 @@ fs =    require('fs');         // required to read certs and keys
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.render('index.html');
-});
+/*app.get('/', function (req, res) {
+  res.setHeader('Content-Type','text/html')
+  res.render('public/index.html');
+});*/
+
+app.use(express.static('public'));
 
 
 var options = {
     key:    fs.readFileSync('private/server.key'),
     cert:   fs.readFileSync('private/server.crt'),
-    requestCert:        true,
-    rejectUnauthorized: false
+//    requestCert:        true,
+//    rejectUnauthorized: false
 };
 
-https.createServer(options, function (req, res) {
+https.createServer(options, app).listen(3250, function () {
 	console.log("Server running")
-    if (req.client.authorized) {
-        res.writeHead(200, '{&quot;Content-Type&quot;: &quot;application/json&quot;}');
-        res.end('{&quot;status&quot;:&quot;approved&quot;}');
-    } else {
-        res.writeHead(401, '{&quot;Content-Type&quot;: &quot;application/json&quot;}');
-        res.end('{&quot;status&quot;:&quot;denied&quot;}');
-    }
-}).listen(3000);
+	
+});
