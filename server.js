3df8e2148b59313026f3703e0f37e495cc4723a8
BleuAzur <seiwert@live.fr>
2015-09-30 16:10:38 +0200

    Update WS P.1 - Non fonctionnel
    Restructuration du code en vue de l'adapter au multijoueur, ajouts de
    divers commentaires.

--- server.js
+++ server.js
@@ -7,30 +7,39 @@ var port = 3250;
 
 app.use(express.static('public'));
 
+// Importation clé et certificat
 var options = {
     key:    fs.readFileSync('private/server.key'),
     cert:   fs.readFileSync('private/server.crt'),
 };
 
+// Création du serveur https
 var server = https.createServer(options, app).listen(port, function () {
 	console.log("Server running");
 });
 
+// Création du webSocket lié au serveur
 var WebSocketServer = require('ws').Server,
 	wss = new WebSocketServer({server: server});
 	
+// Envoie un message à tous les clients
 wss.broadcast = function(data) {
   for (var i in this.clients)
   {
     this.clients[i].send(data);
+	// DEBUG
 	console.log("Broadcast sent");
   }
 };
+// Tableau contenant tous les joueurs - A revoir
+var allSnakes = [];
+
 
 wss.on('connection', function(ws) {
-	console.log("Connection");
+	console.log("Nouvelle connection");
   ws.on('message', function(message) {
-	  console.log("Fonction message");
-    wss.broadcast(message);
+	  // Process client message
   });
+  // Send something to all clients every x time
+  setInterval(wss.broadcast, 1000);
 });
