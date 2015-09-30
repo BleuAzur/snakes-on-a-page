d3b3bdb7747db52e2db609743e8039cff2fa76a2
BleuAzur <seiwert@live.fr>
2015-09-30 17:25:41 +0200

    Compte Rendu TP3 - Avancement Client-Serveur
    Nombreuses modifs pour l'envoi/réception des données, ajout du compte
    rendu du TP. Toujours non fonctionnel mais on semble toucher au but.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var https = require('https'),      // module for https
-fs =    require('fs');         // required to read certs and keys
+// Ajout des diverses librairies et modules utilisées pour mettre en place le serveur
+
+var https = require('https'),
+fs =    require('fs');
 var express = require('express');
 var app = express();
 
@@ -22,24 +24,40 @@ var server = https.createServer(options, app).listen(port, function () {
 var WebSocketServer = require('ws').Server,
 	wss = new WebSocketServer({server: server});
 	
-// Envoie un message à tous les clients
-wss.broadcast = function(data) {
-  for (var i in this.clients)
-  {
-    this.clients[i].send(data);
-	// DEBUG
-	console.log("Broadcast sent");
-  }
-};
-// Tableau contenant tous les joueurs - A revoir
-var allSnakes = [];
 
+// Tableau contenant les données des joueurs
+var allSnakes = [];
+//Tableau contenant tous les joueurs
+var clients = {};
+var count = 0;
+
+// Envoie un message à tous les clients
+
+
+function broadcast(data) {
+	  for (var i in clients)
+	  {
+		 //DEBUG
+		  console.log(JSON.stringify(data));
+		 // On envoie allSnakes à chaque client
+		clients[i].send(JSON.stringify(data));
+	  }
+	}
 
 wss.on('connection', function(ws) {
+	
+	// DEBUG
 	console.log("Nouvelle connection");
+	// On store la nouvelle connection dans un tableau de clients.
+	clients[id] = ws;
+	// Incrémentation du prochain ID joueur et du nombre de joueurs actuels
+	var id = count++;
+	
   ws.on('message', function(message) {
-	  // Process client message
+	  allSnakes.push(JSON.parse(message));
   });
+  
+  
   // Send something to all clients every x time
-  setInterval(wss.broadcast, 1000);
+  setInterval(broadcast, 500, allSnakes);
 });
