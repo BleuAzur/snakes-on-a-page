b94d456a06f2db1e2349887c6962ac7d45ee40f8
BleuAzur <seiwert@live.fr>
2015-09-30 14:02:27 +0200

    WebSocket bien mis en place
    Client et Serveur communiquent.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ fs =    require('fs');         // required to read certs and keys
 var express = require('express');
 var app = express();
 
+var port = 3250;
+
 app.use(express.static('public'));
 
 var options = {
@@ -10,6 +12,20 @@ var options = {
     cert:   fs.readFileSync('private/server.crt'),
 };
 
-https.createServer(options, app).listen(3250, function () {
+var server = https.createServer(options, app).listen(port, function () {
 	console.log("Server running")
+});
+
+var WebSocketServer = require('ws').Server,
+	wss = new WebSocketServer({server: server});
+	
+wss.broadcast = function(data) {
+  for (var i in this.clients)
+    this.clients[i].send(data);
+};
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    wss.broadcast(message);
+  });
 });
