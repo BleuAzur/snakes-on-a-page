9e11de0c1954fe17e1963ed20e02709497ea9ce5
BleuAzur <seiwert@live.fr>
2015-09-30 11:40:45 +0200

    Revert "Changements sur snake + début WS"
    This reverts commit 8eaa9fad88c1e1a9aa519f0386aee051066afb20.

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-var https = require('https');      // module for https
+var https = require('https'),      // module for https
 fs =    require('fs');         // required to read certs and keys
 var express = require('express');
 var app = express();
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 3250});
 
 app.use(express.static('public'));
 
 var options = {
     key:    fs.readFileSync('private/server.key'),
     cert:   fs.readFileSync('private/server.crt'),
+//    requestCert:        true,
+//    rejectUnauthorized: false
 };
 
 https.createServer(options, app).listen(3250, function () {
-	console.log("Server running");
-});
-
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
-    });
-    ws.send('something');
+	console.log("Server running")
+	
 });
